d7d55ec5baa8c7abacaec15bd51e4d7e50fc4b38
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-09-12 14:10:22 +0530

    uderstaood useRef, useMemo and useCallback

--- week-6/week-6-2/src/App.jsx
+++ week-6/week-6-2/src/App.jsx
@@ -1,30 +1,29 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-import React from 'react'
+import React from "react";
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+const[inputValue,setinputValue]= useState(1);
+
+let count =0;
+for(let i =1; i<=inputValue; i++){
+  count =count +i
+}
+  function Counterbtn() {
+    setCounter(counter +1);
+  }
   return (
     <div>
-      <Todo id={3}/>
+      <input type="text" onChange={function(e){
+        setinputValue(e.target.value)
+      }}  placeholder="enter you number" />
+      <br></br>
+      <p>sum is {inputValue} is {count}</p>
+      <button onClick={() => Counterbtn()}> Counter is {counter} </button>
     </div>
-  )
-}
+  );
+};
 
-function Todo({id}){
-  const [todo , settodo]= useState({});
-   
-  //implement effect here
-  useEffect(()=>{
-axios.get("https://jsonplaceholder.typicode.com/todo?id="+ id).then(Response=>{
-  settodo(Response.data.todo)
-})
-  },[]) 
-
-
-  return<div>
-    <h1>{todo.title}</h1>
-  </div>
-}
-
-export default App
+export default App;
