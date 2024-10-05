53a89f4818c6e776c2fe4d9afb82710706918296
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-10-05 14:13:36 +0530

    adding connnection string

--- week-13-sql-master/src/create-index.ts
+++ week-13-sql-master/src/create-index.ts
@@ -1,12 +1,22 @@
-import { getClient } from "./utils";
+// write a function to create a users table in your database.
+import { Client } from 'pg'
+ 
+const client = new Client({
+  connectionString: "postgresql://db-rishabh_owner:JQzPSNc0aD2x@ep-divine-mud-a5gvqwij.us-east-2.aws.neon.tech/db-rishabh?sslmode=require"
+})
 
-async function addIndex() {
-    const client = await getClient();
-    
-    const createIndexQuery = 'CREATE INDEX idx_todos_user_id ON todos(user_id)';
-    await client.query(createIndexQuery);
-    
-    console.log("Index added successfully on user_id column of todos table!");
+async function createUsersTable() {
+    await client.connect()
+    const result = await client.query(`
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(50) UNIQUE NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        );
+    `)
+    console.log(result)
 }
 
-addIndex();
+createUsersTable();
