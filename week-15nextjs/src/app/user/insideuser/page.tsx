fdb9935256f89147356d0a75bf234e5b348ba857
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-12-17 21:01:18 +0530

    calling backend Api without useEffect

--- week-15nextjs/src/app/user/insideuser/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function InsideUser(){
-    return <div>hii there i'm the inside the user </div>
-}
--- week-15nextjs/src/app/user/page.tsx
+++ week-15nextjs/src/app/user/page.tsx
@@ -1,32 +1,51 @@
 
-"use client"
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-export default function User() {
+// export default function User() {
 
-    const[data, setData]=useState({
-        name:"",
-        email:""
-    })
-    const [loading,setLoading]=useState(false)
-  useEffect(() => {
-    axios
-      .get(
-        "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
-      )
-      .then((res) => {
-        setData(res.data)
-      });
-  }, []);
+//     const[data, setData]=useState({
+//         name:"",
+//         email:""
+//     })
+//     const [loading,setLoading]=useState(false)
+//   useEffect(() => {
+//     axios
+//       .get(
+//         "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
+//       )
+//       .then((res) => {
+//         setData(res.data)
+//       });
+//   }, []);
 
-  if(loading) { 
-    return(
-        "ha ha load horha hai"
-    ) 
-  }
-  return <div>
-    {data.name} {""}
+//   if(loading) {
+// return
+//         "ha ha load horha hai" 
+//   }
+//   return <div>
+//     {data.name} {""}
+//     {data.email}
+//   </div>;
+// }
+
+
+//without using use Effect
+
+async function fetchData() {
+    
+    const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+
+    await new Promise(response=>setTimeout(response,4000))
+    console.log(response.data)
+    return(response.data)
+}
+
+export default async function User(){
+ const data = await fetchData()
+
+ return <div>
+    {data.name}
     {data.email}
-  </div>;
+ </div>
 }
